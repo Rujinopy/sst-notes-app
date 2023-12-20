0fccf7cb35b31100c6d265df2a29424d46a92e39
Rujinopy <ruth3442@gmail.com>
2023-12-21 01:54:18 +0700

    Adding unit tests

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,15 +1,15 @@
-import { Api, StackContext, use } from "sst/constructs";
-import { StorageStack } from "./StorageStack";
+import { Api, StackContext, use, Config } from "sst/constructs";
+import { StorageStack, } from "./StorageStack";
 
 export function ApiStack({ stack }: StackContext) {
   const { table } = use(StorageStack);
-
+  const STRIPE_SECRET_KEY = new Config.Secret(stack, "STRIPE_SECRET_KEY");
   // Create the API
   const api = new Api(stack, "Api", {
     defaults: {
       authorizer: "iam",
       function: {
-        bind: [table],
+        bind: [table, STRIPE_SECRET_KEY],
       },
     },
     routes: {
@@ -18,6 +18,7 @@ export function ApiStack({ stack }: StackContext) {
       "GET /notes": "packages/functions/src/list.main",
       "PUT /notes/{id}": "packages/functions/src/update.main",
       "DELETE /notes/{id}": "packages/functions/src/delete.main",
+      "POST /billing": "packages/functions/src/billing.main",
     },
   });
 
@@ -25,6 +26,7 @@ export function ApiStack({ stack }: StackContext) {
   stack.addOutputs({
     ApiEndpoint: api.url,
   });
+  
 
   // Return the API resource
   return {
